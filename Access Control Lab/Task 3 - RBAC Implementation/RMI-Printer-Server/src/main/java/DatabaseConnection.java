1fe2033a9350d11af1c371236bec0c49ee999194
Nikos Karavasilis <nikoskaravasilis10@gmail.com>
2022-11-01 10:24:57 +0100

    apply small changes to db

--- Access Control Lab/Task 3 - RBAC Implementation/RMI-Printer-Server/src/main/java/DatabaseConnection.java
+++ Access Control Lab/Task 3 - RBAC Implementation/RMI-Printer-Server/src/main/java/DatabaseConnection.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 public class DatabaseConnection {
 
-    //Source : https://docs.microsoft.com/en-us/sql/connect/jdbc/step-3-proof-of-concept-connecting-to-sql-using-java?view=sql-server-ver15
+    // Source :
+    // https://docs.microsoft.com/en-us/sql/connect/jdbc/step-3-proof-of-concept-connecting-to-sql-using-java?view=sql-server-ver15
 
-    private static String connectionUrl =
-            "jdbc:sqlserver://dtucourses.database.windows.net:1433;"
-                    + "database=AuthLab;"
-                    + "user=DataSecurity;"
-                    + "password=DS12346!;"
-                    + "encrypt=true;"
-                    + "trustServerCertificate=false;"
-                    + "loginTimeout=30;";
+    private static String connectionUrl = "jdbc:sqlserver:<URL>;"
+            + "database=<database>;"
+            + "user=<user>;"
+            + "password=<password>!;"
+            + "encrypt=true;"
+            + "trustServerCertificate=false;"
+            + "loginTimeout=30;";
 
     public static Boolean userAuthenticated(String Username, String Password) {
-        
+
         ResultSet resultSet = null;
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement();) {
+                Statement statement = connection.createStatement();) {
 
-            String selectSql = ("SELECT COUNT (*) FROM dbo.UsersTask WHERE dbo.UsersTask.Username='"+Username+"' AND dbo.UsersTask.Password='"+Password+"';");
-
+            String selectSql = ("SELECT COUNT (*) FROM dbo.UsersTask WHERE dbo.UsersTask.Username='" + Username
+                    + "' AND dbo.UsersTask.Password='" + Password + "';");
 
             resultSet = statement.executeQuery(selectSql);
             String count = "0";
@@ -36,24 +36,25 @@ public class DatabaseConnection {
 
             }
 
-
-            return (Integer.parseInt(count)!=0);
+            return (Integer.parseInt(count) != 0);
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public static List<String> getAccessControl(String Username) { ResultSet resultSet = null;
+    public static List<String> getAccessControl(String Username) {
+        ResultSet resultSet = null;
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement();) {
+                Statement statement = connection.createStatement();) {
 
-            String selectSql = ("SELECT AccessRight FROM dbo.AccessControlTask WHERE dbo.AccessControlTask.Username='"+Username+"';");
-
+            String selectSql = ("SELECT AccessRight FROM dbo.AccessControlTask WHERE dbo.AccessControlTask.Username='"
+                    + Username + "';");
 
             resultSet = statement.executeQuery(selectSql);
-            List<String> accessRight = new ArrayList<String>() {};
+            List<String> accessRight = new ArrayList<String>() {
+            };
             while (resultSet.next()) {
                 accessRight.add(resultSet.getString(1));
                 System.out.println(accessRight);
@@ -62,10 +63,10 @@ public class DatabaseConnection {
             return accessRight;
         } catch (SQLException e) {
             System.out.println(e.toString());
-            List<String> accessRight = new ArrayList<String>() {};
+            List<String> accessRight = new ArrayList<String>() {
+            };
             accessRight.add("User");
             return accessRight;
         }
     }
 }
-
--- Access Control Lab/Task 4 - ACL Implementation/RMI-Printer-Server/src/main/java/DatabaseConnection.java
+++ Access Control Lab/Task 4 - ACL Implementation/RMI-Printer-Server/src/main/java/DatabaseConnection.java
@@ -3,26 +3,26 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    //Source : https://docs.microsoft.com/en-us/sql/connect/jdbc/step-3-proof-of-concept-connecting-to-sql-using-java?view=sql-server-ver15
+    // Source :
+    // https://docs.microsoft.com/en-us/sql/connect/jdbc/step-3-proof-of-concept-connecting-to-sql-using-java?view=sql-server-ver15
 
-    private static String connectionUrl =
-            "jdbc:sqlserver://dtucourses.database.windows.net:1433;"
-                    + "database=AuthLab;"
-                    + "user=DataSecurity;"
-                    + "password=DS12346!;"
-                    + "encrypt=true;"
-                    + "trustServerCertificate=false;"
-                    + "loginTimeout=30;";
+    private static String connectionUrl = "jdbc:sqlserver:<URL>;"
+            + "database=<database>;"
+            + "user=<user>;"
+            + "password=<password>!;"
+            + "encrypt=true;"
+            + "trustServerCertificate=false;"
+            + "loginTimeout=30;";
 
     public static Boolean userAuthenticated(String Username, String Password) {
-        
+
         ResultSet resultSet = null;
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
-             Statement statement = connection.createStatement();) {
+                Statement statement = connection.createStatement();) {
 
-            String selectSql = ("SELECT COUNT (*) FROM dbo.Users WHERE dbo.Users.Username='"+Username+"' AND dbo.Users.Password='"+Password+"';");
-
+            String selectSql = ("SELECT COUNT (*) FROM dbo.Users WHERE dbo.Users.Username='" + Username
+                    + "' AND dbo.Users.Password='" + Password + "';");
 
             resultSet = statement.executeQuery(selectSql);
             String count = "0";
@@ -34,12 +34,10 @@ public class DatabaseConnection {
 
             }
 
-
-            return (Integer.parseInt(count)!=0);
+            return (Integer.parseInt(count) != 0);
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 }
-
